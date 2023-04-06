c904e75eddcea4c1324f9fd65ae7075f1a0c606a
Nicolas Muckensturm <Nicolas Muckensturm@nicolas.muckensturm@etu.univ-amu.fr>
2023-04-06 16:02:38 +0200

    fix: add local db with cors patch problem

--- migrations/Version20230406140132.php
+++ migrations/Version20230406140132.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230406084710 extends AbstractMigration
+final class Version20230406140132 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,12 @@ final class Version20230406084710 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE todo_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE todo (id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE todo_id_seq CASCADE');
         $this->addSql('DROP TABLE todo');
     }
 }
