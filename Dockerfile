e5c35ebc0e0fd7959ade214b9191904b0eb9c79f
Nicolas Muckensturm <Nicolas Muckensturm@nicolas.muckensturm@etu.univ-amu.fr>
2023-04-06 10:50:38 +0200

    feat: add todo entity & migration

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+FROM php:8.2-apache
+
+ARG DATABASE_URL
+
+# Install required packages
+RUN apt-get update && apt-get install -y \
+    git \
+    unzip \
+    libicu-dev \
+    libpq-dev \
+    libzip-dev \
+    && docker-php-ext-install \
+    intl \
+    pdo_mysql \
+    pdo_pgsql \
+    zip
+
+# Install Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Enable Apache modules
+RUN a2enmod rewrite headers
+
+# Set the working directory
+WORKDIR /var/www/html
+
+# Copy the application code
+COPY . /var/www/html
+
+# Install dependencies
+RUN composer install --optimize-autoloader
+
+RUN php bin/console doctrine:migrations:diff
+
+# Expose port 8000
+EXPOSE 8000
+
+# Start the Apache server
+CMD ["apache2-foreground"]
---
+++ src/Entity/Todo.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TodoRepository::class)]
+#[ApiResource]
+class Todo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
